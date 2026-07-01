fe897efb15627f40b38430f4c6c58a1b48ce8804
ChristianMarcel-bob <christian.m.soares@aluno.senai.br>
2026-07-01 17:27:55 -0300

    inserir map

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,46 @@
 import Header from "./components/Header";
 import Login from "./components/login";
-import CardProd from "./components/Cardprod"; 
+import CardProd from "./components/Cardprod";
 import Contador from "./components/Contador";
 import Calculadora from "./components/Calculadora";
 import "./App.css";
 
-function App (){
+function App() {
+
+  const lanches = [{ id: 1, nome: "X-Salada", preco: "17,50" },
+  { id: 2, nome: "X-Burger", preco: "15,50" },
+  { id: 3, nome: "Refrigerante", preco: "6,00" }
+  ]
   return (
     <>
-     <Header titulo="Lanchonete Xpress"
-     subtitulo="O Melhor da Região" />
+      <Header titulo="Lanchonete Xpress"
+        subtitulo="O Melhor da Região" />
 
-<Login />
+      <Login />
+      {
+        lanches.map(
+          (lanche => (
+            <CardProd
+              key={lanche.id}
+              nome={lanche.nome}
+              preco={lanche.preco}
+            />
+          )
+          ))
+}
 
-<CardProd nome="X-Burger"
+      {/* <CardProd nome="X-Burger"
 preco="15,50" />
 <CardProd nome="X-Salada"
-preco="17,90"/>
+preco="17,50"/>
 <CardProd nome="Refrigerante"
 preco="6,00"/>
 <CardProd nome="João"
-cargo="Atendente"/>
+cargo="Atendente"/> */}
 
-<Contador />
+      <Contador />
 
-<Calculadora />
+      <Calculadora />
 
 
 
---
+++ src/components/Exercícios/arrayNumeros.js
@@ -0,0 +1,8 @@
+const numeros = [2, 5, 10, 20];
+
+const dobro = numeros.map(function(numero) {
+    return numero * 2;
+});
+
+console.log(dobro);
+
